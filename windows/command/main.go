34c60d3c27ed77ece6313060765625c5944cd111
styeer <styeer@email.com>
2024-07-27 01:41:49 +0800

    新增ExecServer方法

--- windows/command/main.go
+++ windows/command/main.go
@@ -1,7 +1,10 @@
 package command
 
 import (
+	"bytes"
 	"errors"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -19,7 +22,22 @@ func CpuName() (string, error) {
 	return s[1], nil
 }
 
-// func main() {
-// 	name, _ := CpuName()
-// 	fmt.Println(name)
-// }
+func ExecServer() (string, error) {
+	path, _ := os.Executable()
+	cmd1 := exec.Command("wmic", "service", "get", "name,pathname")
+	cmd2 := exec.Command("findstr", "/I", "/C:"+path)
+	cmd2.Stdin, _ = cmd1.StdoutPipe()
+	var out bytes.Buffer
+	cmd2.Stdout = &out
+	cmd2.Stderr = os.Stderr
+	cmd2.Start()
+	cmd1.Run()
+	cmd2.Wait()
+	service := strings.Fields(strings.TrimSpace(out.String()))
+	if len(service) == 0 {
+		//fmt.Println("Service does not exist")
+		log.Fatal("Service not fund")
+		return "", errors.New("service not found")
+	}
+	return service[0], nil
+}
